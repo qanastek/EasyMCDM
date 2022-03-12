62da4b4a938486aa6e7d5d674c8bb61d5bf5bc04
sona <quentin.raymondaud84700@gmail.com>
2022-03-12 04:07:31 +0100

    Update Weighted sum

--- EasyMCDM/models/WSum.py
+++ EasyMCDM/models/WSum.py
@@ -10,22 +10,26 @@ class WSum(MCDM):
 
     # Solve the problem by returning a ranking in 0(n)
     def solve(self, pref_indexes: List[int], prefs: List[str], weights: List[float], target='min') -> Dict:
-        keys=[key for key in self.matrix.keys() ]
+        keys=[key for key in self.matrix.keys()]
         assert len(pref_indexes) == len(weights)," - WSum.solve() - weights size doesn't match threshold_indexes size"
         assert len(pref_indexes) == len(prefs)," - WSum.solve() - prefs size doesn't match pref_indexes size"
 
-        weights = [ weights[i] if prefs[i] == target else weights[i]*-1 for i in range(len(weights)) ]
-        matrix = np.asarray([np.array(val) for val in self.matrix.values()])
-        matrix = matrix[:, pref_indexes]
+        # Adjusting weights
+        weights = [weights[i] if prefs[i] == target else weights[i]*-1 for i in range(len(weights))]
 
-        res = []
-        for i in range(len(matrix)):
-            mul = np.multiply(matrix[i], weights)
-            res.append(np.sum(mul))
+        # Get only prefered index columns in data
+        matrix = np.asarray([np.array(val)[pref_indexes] for val in self.matrix.values()])
+
+        # Weighted Summing
+        res = [np.sum(line) for line in np.multiply(matrix[:], [weights for i in self.matrix])]
+        
+        # Shift Index sort on weighted sum 
         argres = np.asarray(res).argsort() if target=='min' else (-np.asarray(res)).argsort()
 
-        res = [ res[index] for index in argres ]
-        ranks = [ i for i in range(1,len(argres)+1)]
-        labels = [ keys[index] for index in argres]
-        scores = [ (rank, key, score) for key, score, rank in zip( labels, res, ranks ) ]
+        # Sorting results
+        res = [res[index] for index in argres]
+        ranks = [i for i in range(1,len(argres)+1)]
+        labels = [keys[index] for index in argres]
+        scores = [(rank, key, score) for key, score, rank in zip(labels, res, ranks)]
+
         return scores
